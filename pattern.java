8d7cb095b112850aa1f1da05a320facdc3b20cbd
Nithyasri <tnithyasri006@gmail.com>
2026-06-09 11:27:05 +0530

    responsive design

---
+++ pattern.java
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Website</title>
+        <style>
+            body{
+                color: blue;
+            }
+            </style>
+    </head>
+    <body>
+        <p>This is my first class</p>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <img src = "download.png">
+        <video controls>
+            <source src = "video.mp4" type="video/mp4">
+        </video>
+        <div>
+        <table>
+            <tr>
+                <th>1</th>
+                <th>2</th>
+            </tr>
+            <tr>
+                <td>1st row 1st col</td>
+                <td>1st row 2nd col</td>
+            </tr>
+            <tr>
+                <td>2nd row 1st col</td>
+                <td>2nd row 2nd col</td>
+            </tr>
+        </table>
+        <ol>
+            <li>1st point</li>
+            <li>2nd point</li>
+        </ol>
+        <ul>
+            <li>1st point</li>
+            <li>2nd point</li>
+        </ul>
+        <header></header>
+        <footer></footer>
+        <nav>
+            <a href = "https://google.com">Google</a>
+            <a href="https://amazon.in">Amazon</a>
+        </nav>
+        <br>
+        <form>
+            <label>Email: </label>
+            <input type = "email"><br>
+            <label>Password: </label>
+            <input type = "password"><br>
+            <label>Gender: </label><br>
+            <input type="radio" name = "gender">Male<br>
+            <input type="radio" name = "gender">Female<br>
+            <label>Event Type: </label>
+            <select>
+                <option>Festival</option>
+                <option>Birthday</option>
+                <option>Other</option>
+            </select><br>
+            <label>Hobbies: </label><br>
+            <input type="checkbox" name = "hobbies">Basketball<br>
+            <input type="checkbox" name = "hobbies">Singing<br>
+            <textarea rows="4" cols="3"></textarea><br>
+            <button>Submit</button>
+        </form>
+    </div>
+        <section></section>
+    </body>
+</html>
